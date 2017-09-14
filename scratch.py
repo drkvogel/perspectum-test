f106d26e1d5bb6d8ecb789608cc547c2546699bc
Chris @ q108-vlubuntu <cbird@q108-vlubuntu>
2017-09-14 13:10:01 +0100

    WIP

---
+++ scratch.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+#!/usr/bin/env python
+
+'''
+Brief - Test Lead (Python)
+
+Task Implementation
+
+Write a script containing a Python function which accepts any number of lists
+ (1..n) as parameters, assuming the lists only contain character strings (e.g. ['a', 'b', 'bc']).
+
+The function should print out the following: -
+    Strings that appear in more than one list -
+    The total number of unique strings across all lists -
+    The total number of strings that were processed
+
+Test Case
+
+Write at least one test case, ensuring the example case below is verified:
+
+Input
+['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn']
+
+Output
+
+Strings appearing in multiple lists: 'gh'
+Number of unique strings: 7
+Total number of strings processed: 9
+
+Please use either Bitbucket/GitHub, or provide a ZIP containing the application code
+'''
+
+# print "Hello, World!"
+import sys # for sys.stdout.write
+
+def analyse_lists(*lists):
+    # count = 0
+    # more_than_once = 0
+    # more_than_once = []
+    all_strings = []
+    list_unique = {}
+    occurrence_count = {}
+    for lista in lists:
+        # print list
+        for el in lista:
+            # print el
+            # count = count + 1
+            if el not in occurrence_count.keys():
+                occurrence_count[el] = 1
+            else:
+                occurrence_count[el] = occurrence_count[el] + 1
+            all_strings.append(el)
+            # if el in all_strings and el not in more_than_once:
+            #     # more_than_once = more_than_once + 1 # incorrect, will count too many if el in 3 or more lists
+            #     more_than_once.append(el) # wrong
+    list_unique = set(all_strings)
+
+    # for list in lists:
+    # #     # print list
+    #     list = set(list)
+    # for i, lista in enumerate(lists):
+        # lists[i] = set(lista) #set(list)
+    # print(list) # <type 'list'>
+    # temp = set()
+    temp = list()
+    for i in range(0, len(lists)):
+        # lists[i] = list(lists[i]) #set(list)
+        # temp.add(set(lists[i]))
+        temp.append(set(lists[i]))
+        # print set(lists[i])
+        # print list(lists[i])
+        # lists[i] = set(lists[i])
+        # lists[i] = list(lists[i])
+
+    # for i in range(len(temp))
+    occurrence_count = dict()
+
+    for lista in temp:
+        # print 'lista: ' + str(lista)
+        for el in lista:
+            if el not in occurrence_count.keys():
+                occurrence_count[el] = 1
+            else:
+                occurrence_count[el] = occurrence_count[el] + 1
+
+    # for el in list:
+    #         print el
+    #         count = count + 1
+    #         all_strings.append(el)
+
+    print 'Input: ' + str(lists)
+    # print 'all_strings: ' + str(all_strings)
+    # still not right as two or more identical strings in the same list is a false positive
+    print 'len(occurrence_count): ' + str(len(occurrence_count))
+    print 'Strings appearing in multiple lists: ',# + str(more_than_once)
+    # for key in occurrence_count:
+    #     if occurrence_count[key] > 1:
+    #         print "'" + key + "', ",
+    for i, key in enumerate(occurrence_count):
+        if occurrence_count[key] > 1:
+            if i > 0:
+                # print (', ', end='') # python 3
+                # print ', ',
+                sys.stdout.write(', ')
+            # print(key, end='')
+            sys.stdout.write('\'' + key + '\'')
+        #     print "'" + key + "', ",
+
+            # if i == len(occurrence_count) - 1:
+            #     print "'" + key + "', "
+            # else:
+            #     print "'" + key + "', ",
+    print
+    print 'Number of unique strings: ' + str(len(list_unique))
+    # print 'Strings processed: ' + str(count)
+    print 'Strings processed: ' + str(len(all_strings))
+    print
+
+analyse_lists(['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn'])
+analyse_lists(['g', 'gh', 'ghj', 'g', 'hh'], ['j', 'ju', 'gh', 'gk', 'gn'], ['g', 'ju', 'hh', 'hh'])
